ab4060c61aa0d67d462ec24ae54c10888d7df154
DESKTOP-OKQQ9TH\Cong <dangcong1810@gmail.com>
2021-05-05 01:01:24 +0700

    xong laravel api

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\OrderResource;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function index(){
-       return Order::all();
+       return OrderResource::collection(Order::with('orderItems')->get());
     }
 }
